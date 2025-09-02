053b49e0db0dfa474d0f6dba872965ca7c1350c2
Matilde Morrone <contact@morrone.dev>
2025-09-02 13:46:58 +0200

    Configure tanstack, prettier and tailwindcss

--- client/astro.config.ts
+++ client/astro.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from 'astro/config';
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
---
+++ client/components/Form.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Form: Component = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Form;
